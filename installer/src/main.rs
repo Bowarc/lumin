9fd172f481d6e3a9293b337b44ef7d2bc318be78
Bowarc <yuyeaaaahhhh@laposte.net>
2023-05-05 02:22:41 +0200

    Adding debug releases listing

--- installer/src/main.rs
+++ installer/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write as _;
+
 mod models;
 const REPO_URL: &str = "github.com/Bowarc/Lumin";
 const GH_API_RELEASE_URL: &str = "https://api.github.com/repos/bowarc/lumin/releases";
@@ -64,10 +66,21 @@ fn download_latest_release(mut target_path: std::path::PathBuf) {
         .json::<Vec<models::gh_releases::Release>>()
         .unwrap();
 
+    println!("Listing releases");
+    for (idx, release) in releases.iter().enumerate() {
+        println!(
+            "Release {idx}\nName: {}\nTag: {}\nDescription: {}\nDate: {}\n\n",
+            release.name, release.tag_name, release.body, release.published_at
+        )
+    }
+
     let latest_release = releases.get(1).unwrap();
     println!("Downloading assets ...");
-    for asset in &latest_release.assets {
-        println!("Downloading {} .. ", asset.name);
+    for asset in latest_release.assets.iter() {
+        let start_time = std::time::Instant::now();
+        print!("Downloading {} ..\r", asset.name);
+        std::io::stdout().flush().unwrap();
+
         let resp = client
             .get(asset.browser_download_url.clone())
             .send()
@@ -76,9 +89,18 @@ fn download_latest_release(mut target_path: std::path::PathBuf) {
         let body = resp.bytes().expect("body invalid");
 
         std::fs::write(target_path.join(asset.name.clone()), &body).unwrap();
+        println!(
+            "Downloading {} .. Done ({} bytes in {:.2}s)",
+            asset.name,
+            body.len(),
+            start_time.elapsed().as_secs_f32()
+        );
     }
 
     println!("Done");
+
+    // ask to create a lnk on desktop
+    // ask to create an entry in the start menu dir
 }
 
 fn main() {
